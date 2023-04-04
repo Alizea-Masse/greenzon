ab6855bfb0e3dab9e873fa5a1b73d763429d21e9
Alizéa Massé <alizea777@hotmail.fr>
2023-04-04 10:03:16 +0200

    complete header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,8 +9,8 @@ const Header = () => {
   return (
     <header>
       {/* top nav */}
-      <div className="flex items-center bg-greenzon_green p-1 flex-grow py-2">
-        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+      <div className="flex items-center bg-greenzon_green  flex-grow ">
+        <div className="mt-1 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="/greenzonnobg.png"
             width={150}
@@ -20,9 +20,9 @@ const Header = () => {
           />
         </div>
         {/* Search */}
-        <div className="hidden sm:flex bg-yellow-400 hover:bg-yellow-500 items-center h-10 rounded-md flex-grow cursor-pointer">
+        <div className="hidden sm:flex bg-yellow-400 hover:bg-yellow-500 items-center h-8 rounded-md flex-grow cursor-pointer">
           <input
-            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"
+            className=" h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"
             type="text"
           />
           <BsSearchHeart className="h-12 m-4" />
@@ -38,7 +38,7 @@ const Header = () => {
             <p className="font-extrabold md:text-sm">& Commandes</p>
           </div>
           <div className="link relative flex items-center">
-            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 rounded-full text-center text-black font-bold">
+            <span className="absolute top-2 left-9 md:right-10 h-4 w-4 bg-yellow-400 rounded-full text-center text-black font-bold">
               0
             </span>
             <FiShoppingCart size={32} className="" />
@@ -49,12 +49,23 @@ const Header = () => {
         </div>
       </div>
       {/* bottom nav */}
-      <div className="flex items-center bg-greenzon_green-light">
-        <p>
-          <AiOutlineMenu className="h-6
-           mr-1" />
-           Tous
+      <div className="flex items-center space-x-3 bg-greenzon_green-light text-white text-sm">
+        <p className="link flex items-center">
+          <AiOutlineMenu component={'span'} size={35} className="h-6 mr-1" />
+          Tous
         </p>
+        <p className="link">Meilleures ventes</p>
+        <p className="link">Service Client</p>
+        <p className="link">Dernières Nouveautés</p>
+        <p className="link hidden lg:inline-flex">Basics</p>
+        <p className="link hidden lg:inline-flex">Musique</p>
+        <p className="link hidden lg:inline-flex">Ventes</p>
+        <p className="link hidden lg:inline-flex">Ebooks</p>
+        <p className="link hidden lg:inline-flex">Audible</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Livres</p>
+        <p className="link hidden lg:inline-flex">Maison</p>
+        <p className="link hidden lg:inline-flex">Informatique</p>
       </div>
       <div></div>
     </header>
