bc7bf2b4c5a434d48fdcb697bf01c4acea9283b7
Alizéa Massé <alizea777@hotmail.fr>
2023-04-04 11:29:07 +0200

    add banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="relative">
+        <div className="absolute w-full h-7 bg-gradient-to-t from-white to-transparent bottom-0 z-20" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <Image
+            loading="lazy"
+            src="/banner1.png"
+            alt=""
+            width={800}
+            height={300}
+            unoptimized={true}
+          />
+        </div>
+        <div>
+          <Image
+            loading="lazy"
+            src="/banner2.png"
+            alt=""
+            width={800}
+            height={300}
+            unoptimized={true}
+          />
+        </div>
+        <div>
+          <Image
+            loading="lazy"
+            src="/banner3.png"
+            alt=""
+            width={800}
+            height={300}
+            unoptimized={true}
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,21 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-green-100">
       <Head>
         <title>Greenzon</title>
       </Head>
       {/* header  */}
       <Header/>
-      <main>
+      <main className="max-w-screen-2xl mx-auto">
         {/* Banner */}
         <Banner/>
-        {/* Product field */}
+        {/* Product feed */}
+        <ProductFeed/>
       </main>
       
     </div>
