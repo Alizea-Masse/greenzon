d861b0b968bf23a4ee6c203dca102927e6275b53
Alizéa Massé <alizea777@hotmail.fr>
2023-04-05 10:39:54 +0200

    calcul total and chekout part

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     images: {
         domains: ['links.papareact.com','fakestoreapi.com','www.canva.com'],
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.resolve.fallback.fs = false;
+        }
+        return config;
+      },
     }
 }
